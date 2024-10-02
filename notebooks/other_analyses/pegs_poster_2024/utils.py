b9c16ef9d18cd74881c28fa49bd8c31c9b2571d0
Leandro Radusky <leandro.radusky@gmail.com>
2024-10-02 15:47:13 +0200

    PEGS poster figures and analyses final

--- notebooks/other_analyses/pegs_poster_2024/utils.py
+++ notebooks/other_analyses/pegs_poster_2024/utils.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import matplotlib.colors as mcolors
+from scipy.stats import shapiro, kruskal
 
 def load_and_clean_data(file_path, sheet_name='Sheet1'):
     df = pd.read_excel(file_path, sheet_name=sheet_name)
@@ -25,40 +26,10 @@ def posthoc_dunn_test(df, group_col='Condition', value_col='Concentration'):
 def posthoc_conover_test(df, group_col='Condition', value_col='Concentration'):
     return sp.posthoc_conover(df, val_col=value_col, group_col=group_col, p_adjust='bonferroni')
 
-def plot(df, bmk, func, palette, group_col='Condition', value_col='Concentration', results_dir='results'):
-    fig = plt.figure(figsize=(10, 6))
-    func(x=group_col, y=value_col, data=df, palette=palette)
-    plt.xticks(rotation=45)
-    plt.xlabel(group_col)
-    plt.ylabel(f'$log([{bmk}]/[Total Protein])$')
-    plt.tight_layout()
-    plt.savefig(os.path.join(results_dir, f'{bmk}_{func.__name__}.svg'), format='svg')
-    plt.close(fig)
-
-def plot_heatmap_medians(df, bmk, group_col='Condition', value_col='Concentration', results_dir='results'):
-    custom_palette = sns.color_palette("husl", 256)  
-    pink_to_purple_palette = custom_palette[192:256]  
-    cmap = mcolors.LinearSegmentedColormap.from_list("pink_purple_husl", pink_to_purple_palette)
-
-    median_concentrations = df.groupby('Condition')['Concentration'].median().reset_index()
-
-    condition_order = ['Reference', '24h 4ºC +PBS', '24h 4ºC -PBS', '72h 4ºC +PBS', '72h 4ºC -PBS']
-    median_concentrations['Condition'] = pd.Categorical(median_concentrations['Condition'], categories=condition_order, ordered=True)
-    median_concentrations = median_concentrations.sort_values('Condition')
-    
-    fig = plt.figure(figsize=(8, 4))
-    sns.heatmap(median_concentrations.set_index(group_col).T, annot=True, fmt='.2f', cmap=cmap)
-    plt.title('Heatmap of Median Concentrations by Condition')
-    plt.xlabel(group_col)
-    plt.tight_layout()
-    plt.savefig(os.path.join(results_dir, f'{bmk}_heatmap_medians.svg'), format='svg')
-    plt.close(fig)
-
-def plot_raincloud(df, bmk, results_dir):
-    plt.figure(figsize=(10, 6))
-    pt.RainCloud(x='Condition', y='Concentration', data=df, palette="husl", bw=0.2, width_viol=0.6, ax=None, orient="h", alpha=0.65)
-    plt.xlabel(f'$log([{bmk}]/[Total Protein])$')
-    plt.tight_layout()
-    plt.savefig(os.path.join(results_dir, f'{bmk}_raincloud_plot.svg'), format='svg')
-    plt.close()
-
+def shapiro_wilk_test(df, value_col, group_col):
+    shapiro_results = {}
+    for condition in df[group_col].unique():
+        condition_data = df[df[group_col] == condition][value_col]
+        stat, p_value = shapiro(condition_data)
+        shapiro_results[condition] = {'Statistic': stat, 'p-value': p_value, 'Normal': p_value > 0.05}
+    return shapiro_results
